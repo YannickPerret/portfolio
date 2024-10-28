c964fccf7c4d6d9883890dfeb9993fb1ff03b50a
tchoune <contact@yannickperret.com>
2024-10-28 19:01:52 +0100

    chore : make scroll backend transition

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,15 +1,47 @@
+'use client'
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
 const Navbar = () => {
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 50) {
+                setIsScrolled(true);
+            } else {
+                setIsScrolled(false);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        // Nettoyage du listener quand le composant est démonté
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
-        <nav className="fixed w-full bg-white shadow z-50">
+        <nav
+            className={`fixed w-full z-50 transition-all duration-500 ${
+                isScrolled ? 'bg-white shadow-md' : 'bg-transparent'
+            }`}
+        >
             <div className="container mx-auto px-6 py-4 flex justify-between items-center">
-                <div className="text-2xl font-bold text-blue-600">Perret Yannick</div>
+                <div className={`text-2xl font-bold transition-all duration-500 ${isScrolled ? 'text-blue-600' : 'text-white'}`}>
+                    Perret Yannick
+                </div>
                 <ul className="flex space-x-6">
                     {['Accueil', 'À Propos', 'Projets', 'Compétences', 'Contact'].map((item, index) => (
                         <li key={index}>
-                            <Link href={`#${item.toLowerCase().replace(' ', '')}`} className="text-gray-600 hover:text-blue-500">
-                              {item}
+                            <Link
+                                href={`#${item.toLowerCase().replace(' ', '')}`}
+                                className={`transition-all duration-500 ${
+                                    isScrolled ? 'text-gray-700 hover:text-blue-500' : 'text-gray-200 hover:text-blue-300'
+                                }`}
+                            >
+                                {item}
                             </Link>
                         </li>
                     ))}
